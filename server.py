6dd0688df82c9eb654203311e08221db0260bb69
RaedRdhaounia <raedrdhaounia@gmail.com>
2024-01-25 21:29:34 +0100

    correction health care function for testing server working

--- server.py
+++ server.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def helloword():
-
-    return "hello word"
+def health_care():
+    return {
+        "message": "success connect to the server",
+        "status": True,
+        "statusCode": 200
+    }
 
 app.run(debug = True)
