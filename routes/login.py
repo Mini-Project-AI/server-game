de570b61b2de433ce0c9f5a5f686a29c542c01fd
RaedRdhaounia <raedrdhaounia@gmail.com>
2024-02-01 08:25:39 +0100

    create register and login packages

---
+++ routes/login.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, request
+from werkzeug.security import check_password_hash
+import jwt
+from config.connect import db
+from util.validation_inputs import is_valid_email, is_valid_password
+import datetime
+from util.generate_response import response_bad_request, response_ok, response_not_found
+
+JWT_SECRET = 'your_jwt_secret'
+
+login_blueprint = Blueprint('login', __name__)
+
+@login_blueprint.route("/login", methods=["POST"])
+def login_user():
+
+    data = request.json
+
+    # Validate email
+    if not is_valid_email(data.get("email", "")):
+        return response_bad_request("Invalid email format")
+    
+    # Validate password
+    if not is_valid_password(data.get("password", "")):
+        return response_bad_request("Password must meet the criteria")
+
+    # Fetch user from DB
+    user = db.users.find_one({"email": data["email"]})
+    if not user:
+        return response_not_found("User not found")
+
+    # Verify password
+    if not check_password_hash(user['password'], data['password']):
+        return response_bad_request("Invalid credentials")
+
+    user_data = {
+    "user_id": str(user['_id']),  # Convert ObjectId to string
+    "email": user['email'],
+    "first_name": user['first_name'],
+    "last_name": user['last_name'],
+    }
+    
+    # Generate JWT token
+    token_data = {
+    "email": user["email"],
+    "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=24)
+    }
+    token = jwt.encode(token_data, JWT_SECRET, algorithm="HS256")
+
+    return  response_ok( "user login with success",{"user": user_data, "token": token}) 
---
+++ routes/register.py
@@ -0,0 +1,61 @@
+from flask import Blueprint, request
+
+from werkzeug.security import generate_password_hash
+import jwt
+from config.connect import db
+from util.validation_inputs import is_valid_email, is_valid_password, is_valid_name, is_valid_age
+import datetime
+from util.generate_response import response_bad_request, response_create_request
+# Replace 'your_jwt_secret' with your actual JWT secret key
+JWT_SECRET = 'your_jwt_secret'
+register_blueprint = Blueprint('register', __name__)
+
+@register_blueprint.route("/register", methods=["POST"])
+def register_user():
+    data = request.json
+
+    # Validate email
+    if not is_valid_email(data.get("email", "")):
+        return response_bad_request("Invalid email format")
+    
+    # Validate password
+    if not is_valid_password(data.get("password", "")):
+        return response_bad_request("Password must meet the criteria")
+
+    # Validate first and last name
+    if not is_valid_name(data.get("first_name", "")) or not is_valid_name(data.get("last_name", "")):
+        return response_bad_request("First and last names must be at least 4 characters long")
+
+    # Validate age
+    if not is_valid_age(data.get("age", 0)):
+        return response_bad_request("Age must be between 6 and 60")
+    
+    hashed_password = generate_password_hash(data['password'])
+
+    # Check if user already exists
+    existing_user = db.users.find_one({"email": data["email"]})
+    print(existing_user)
+    if existing_user:
+        return response_bad_request("User already exists")
+
+    # Create new user
+    new_user = {
+        "email": data["email"],
+        "password": hashed_password,
+        "age": data["age"],
+        "first_name": data["first_name"],
+        "last_name": data["last_name"]
+    }
+    insert_result = db.users.insert_one(new_user)
+    
+    new_user.pop("password")
+    new_user["_id"] = str(insert_result.inserted_id)
+
+    # Generate JWT token
+    token_data = {
+        "email": data["email"],
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=24)  # 24 hour expiration
+    }
+    token = jwt.encode(token_data, JWT_SECRET, algorithm="HS256")
+
+    return  response_create_request( "user created with success",{"user": new_user, "token": token}) 
