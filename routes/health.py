69c920da9536fb033888afc7079ae89a9e2c37fe
RaedRdhaounia <raedrdhaounia@gmail.com>
2024-02-01 08:24:24 +0100

    update heath care route with new architecture of api response

--- routes/health.py
+++ routes/health.py
@@ -1,11 +1,8 @@
 from flask import Blueprint
+from util.generate_response import response_bad_request, response_ok
 
 health_blueprint = Blueprint('health', __name__)
 
 @health_blueprint.route("/health-care")
 def health_care():
-    return {
-        "message": "success connect to the server",
-        "status": True,
-        "statusCode": 200
-    }
+    return response_ok("success connect to the server")
